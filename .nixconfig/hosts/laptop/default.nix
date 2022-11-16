c347ffde3a8c42fecac8f1769edcd2790dc3d210
Krizdano <akrishna852@gmail.com>
2022-11-16 16:09:48 +0530

    updated config

--- .nixconfig/hosts/laptop/default.nix
+++ .nixconfig/hosts/laptop/default.nix
@@ -86,9 +86,12 @@
   home-manager = {
     #useUserPackages = true;
     #useGlobalPkgs = true;
-    users.${user} = { config, pkgs, inputs, ... }: {
+    users.${user} = { config, pkgs, inputs, lib,  ... }: {
 
-      imports = [ ../../modules/shell/zsh.nix ];
+      imports = [ 
+        ../../modules/shell/zsh.nix 
+        ../../modules/WindowManagers/sway.nix
+      ];
   # Everything inside here is managed by Home Manager!
 
  # imports  = [ ../../modules/shell/zsh.nix ];
@@ -118,21 +121,11 @@
 
   home.packages = with pkgs; [
    pulseaudio   
-   swaylock
-   swayidle
-   sway-launcher-desktop
-   wl-clipboard
-   mako # notifications
    w3m # terminal browser
    ytfzf # terminal youtube 
    bemenu # search bar
-   wl-clipboard
    fzf
-   wf-recorder
-   sway-contrib.inactive-windows-transparency
    glib # gsettings
-   slurp
-   autotiling #sway autotiling
   ];
   
   # lf terminal filemanager
@@ -282,6 +275,8 @@
     }
 
          ''; }
+
+
 
 
      {     
@@ -493,7 +488,9 @@
   };
 
 
-
+  # wallpaper
+  home.file.".config/wallpapers".source = ../../modules/themes/wallpapers;
+  home.file.".themes".source = ../../modules/themes/.themes;
   # themes
   gtk = {
     enable = true;
@@ -510,144 +507,9 @@
 
 
 
-  # sway 
-  wayland.windowManager.sway = {
-    enable = true; 
-    wrapperFeatures.gtk = true;
     
-    extraSessionCommands = ''export SDL_VIDEODRIVER=wayland
-                             export QT_QPA_PLATFORM=wayland
-                             export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-                             export _JAVA_AWT_WM_NONREPARENTING=1
-                             export MOZ_ENABLE_WAYLAND=1
-                             export WLR_NO_HARDWARE_CURSORS=1
-                           '';
+       
    
-       
-    # sway config
-    config = {  
-      # touchpad config 
-      input = { "1739:32382:PNP0C50:0e_06CB:7E7E_Touchpad" = {
-        dwt = "enable";
-        tap = "enable";
-        natural_scroll = "enabled";
-        middle_emulation = "enabled";
-       };
-      };
-
-      terminal = "kitty";
-      menu = "bemenu-run";
-      modifier = "Mod4";
-      gaps.inner = 7;
-      window.border = 2;
-      colors = {
-        focused = {
-          background = "#ffffff";
-          border = "#ffffff";
-          childBorder = "#BAC2DE";
-          indicator = "#BAC2DE";
-          text = "#ffffff";
-        };
-
-      };
-      # keybindings 
-      keybindings = {     
-       "mod4+b" = "exec firefox"; # opens firefox
-       "mod4+Shift+y" = "exec firefox https://www.youtube.com"; # opens youtube in firefox
-       "mod4+Shift+n" = "exec firefox https://mipmip.github.io/home-manager-option-search/"; # opens home-manager search
-       "mod4+n" = "exec firefox https://search.nixos.org/packages"; # opens nixos search 
-       "mod4+Shift+q" = "kill"; # close windows
-       "mod4+Return" =  "exec kitty"; # open terminal (alacritty)
-       "mod4+d" = "exec kitty --class=launcher -e sway-launcher-desktop"; # sway-launcher-desktop 
-       "mod4+y" = "exec kitty --class=ytfzf -e ytfzf -t --thumb-viewer=kitty -l -s -f --preview-side=right"; # youtube in terminal
-       "mod4+s" = "exec kitty --class=ytfzf -e ytfzf -t --thumb-viewer=kitty -l -s -f -c SI --preview-side=right"; # youtube subscriptions in terminal
-       "mod4+Shift+c" = "reload"; # reload sway
-       
-       # exit sway 
-       "mod4+shift+e" = "exec swaynag - t warning -m 'you pressed the exit shortcut.Do you really want to exit sway?' -B 'yes' 'swaymsg exit'"; 
-       
-       #scratchpad
-       "mod4+Shift+minus" = "move scratchpad";
-       "mod4+minus" = "scratchpad show";
-
-       # moving aroud
-       "mod4+Down" = "focus down";
-       "mod4+Up" = "focus up";
-       "mod4+Right" = "focus right";
-       "mod4+Left" = "focus left";
-       # use vim keys to move around
-       "mod4+j" = "focus down";
-       "mod4+k" = "focus up";
-       "mod4+l" = "focus right";
-       "mod4+h" = "focus left";
-
-       # move focused window
-       "mod4+Shift+Left" = "move left";
-       "mod4+Shift+Down" = "move down";
-       "mod4+Shift+Right" = "move right";
-       "mod4+Shift+Up" = "move up";
-
-       # workspaces
-       "mod4+1" = "workspace number 1";
-       "mod4+2" = "workspace number 2";
-       "mod4+3" = "workspace number 3";
-       "mod4+4" = "workspace number 4";
-       "mod4+5" = "workspace number 5";
-       "mod4+6" = "workspace number 6";
-       "mod4+7" = "workspace number 7";
-       "mod4+8" = "workspace number 8";
-       "mod4+9" = "workspace number 9";
-       # move focused container to workspaces
-       "mod4+Shift+1" = "move container to workspace number 1";
-       "mod4+Shift+2" = "move container to workspace number 2";
-       "mod4+Shift+3" = "move container to workspace number 3";
-       "mod4+Shift+4" = "move container to workspace number 4";
-       "mod4+Shift+5" = "move container to workspace number 5";
-       "mod4+Shift+6" = "move container to workspace number 6";
-       "mod4+Shift+7" = "move container to workspace number 7";
-       "mod4+Shift+8" = "move container to workspace number 8";
-       "mod4+Shift+9" = "move container to workspace number 9";
-      
-       "mod4+f" = "fullscreen"; # full screen
-       "mod4+space" = "focus mode_toggle"; # toggle focus window
-       "mod4+Shift+space" = "floating toggle"; # floating window
-       "XF86MonBrightnessDown" = "exec light -U 10";  # increase increase brightness
-       "XF86MonBrightnessUp" = "exec light -A 10"; # decrease brightness 
-       "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'"; # increase volume
-       "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'"; # decrease volume
-       "XF86AudioMute" = "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'"; # mute
-     };
-
-
-      # swaybar config
-      bars = [{
-       fonts.size = 15.0; 
-       command = "waybar";
-       position = "top";
-       hiddenState = "hide";
-       mode = "hide";
-       extraConfig = "modifier mod4";
-      }];
-       
-      # output      
-      output = {  # external monitor
-       HDMI-A-1 = { 
-        mode = "1366x768";
-        bg =  "/home/krizdavezz/Pictures/wallpapers/three_squares.png fill";        
-        position = "0,1920";
-       };
-      };
-    };
-          
-    # extra configurations
-    extraConfig = 
-    "exec autotiling \n
-     exec inactive-windows-transparency.py \n 
-     output * bg /home/krizdavezz/Pictures/wallpapers/three_squares.png fill \n
-     include /home/krizdavezz/.config/sway/extraconf";
-      
-  }; # end of sway
-      
   # waybar
   programs.waybar = {
     enable = true;   
