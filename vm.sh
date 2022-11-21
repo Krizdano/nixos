4f2a32a0afb0de4994017a2157a88935629fda80
Krizdano <akrishna852@gmail.com>
2022-11-21 12:07:18 +0530

    organised everything

--- vm.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env sh
-
-mkdir $HOME/Downloads
-mkdir $HOME/Pictures 
-pushd $HOME/nixos 
-cp -r wallpapers $HOME/Pictures 
-cp -r .themes $HOME 
-cp sway_extraconf/extraconf $HOME/.config/sway 
-popd
