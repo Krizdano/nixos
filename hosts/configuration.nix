6a2621c983eaa27aafbb74db62326e4620334020
Krizdano <akrishna852@gmail.com>
2022-12-02 13:21:33 +0530

    updated flake.lock

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -191,7 +191,7 @@
   { from = 1741; to = 1764; } ];
   
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  #networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
